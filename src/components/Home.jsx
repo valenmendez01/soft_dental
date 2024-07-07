57f0b79193df2269e6f9afc6e693b2b5410f36c6
valenmendez01 <mendezvalentin824@gmail.com>
2024-07-07 19:57:31 -0300

    com

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,47 @@
-import appFirebase from '../credenciales'
-import {getAuth, signOut} from 'firebase/auth'
-import { Layout } from 'antd';
+import appFirebase from '../credenciales';
+import { getAuth, signOut } from 'firebase/auth';
+import { Button, Layout } from 'antd';
 import Logo from './Logo';
 import MenuList from './MenuList';
+import { useState } from 'react';
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { theme } from 'antd'; // Importar theme de Ant Design
 
-const auth = getAuth(appFirebase)
-const { Header, Sider } = Layout
+const auth = getAuth(appFirebase);
+const { Header, Sider } = Layout;
+
 // eslint-disable-next-line react/prop-types
 const Home = ({ correoUsuario }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const { token: { colorBgContainer } } = theme.useToken();
 
   return (
     <div>
-      <h2>Bienvenido {correoUsuario}<button onClick={() => signOut(auth)}>Log Out</button></h2>
       <Layout>
-        <Sider theme='dark' className='sidebar'>
-          <Logo/>
+        <Sider
+          collapsed={collapsed}
+          collapsible
+          trigger={null}
+          theme='dark'
+          className='sidebar'
+        >
+          <Logo />
           <MenuList />
         </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type='text'
+              className='toggle'
+              onClick={() => setCollapsed(!collapsed)}
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            />
+            <h2>Bienvenido {correoUsuario}<button onClick={() => signOut(auth)}>-Log Out</button></h2>
+          </Header>
+        </Layout>
       </Layout>
     </div>
   );
 };
 
-
-export default Home
+export default Home;
