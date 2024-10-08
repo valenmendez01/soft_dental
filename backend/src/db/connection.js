72371132d672d4ef5612b3612d37c1f4d3de0a3b
valenmendez01 <mendezvalentin824@gmail.com>
2024-10-08 10:47:09 -0300

    gg

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,34 +1,13 @@
 // Configuración de la conexión a la base de datos
 
-const mysql = require('mysql');
-
-const db = mysql.createConnection({
-  host: import.meta.env.VITE_MYSQLHOST,
-  user: import.meta.env.VITE_MYSQLUSER,
-  password: import.meta.env.VITE_MYSQLPASSWORD,
-  database: import.meta.env.VITE_MYSQLDATABASE,
-  port: import.meta.env.VITE_MYSQLPORT
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("Error al conectar a la base de datos:", err);
-  } else {
-    console.log("Conectado a la base de datos");
-  }
-});
-
-module.exports = db;
-
-
-
 // const mysql = require('mysql');
 
 // const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "soft_dental_database"
+//   host: import.meta.env.VITE_MYSQLHOST,
+//   user: import.meta.env.VITE_MYSQLUSER,
+//   password: import.meta.env.VITE_MYSQLPASSWORD,
+//   database: import.meta.env.VITE_MYSQLDATABASE,
+//   port: import.meta.env.VITE_MYSQLPORT
 // });
 
 // db.connect((err) => {
@@ -40,3 +19,24 @@ module.exports = db;
 // });
 
 // module.exports = db;
+
+
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "soft_dental_database"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a la base de datos:", err);
+  } else {
+    console.log("Conectado a la base de datos");
+  }
+});
+
+module.exports = db;
